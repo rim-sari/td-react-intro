db3c2aa2e6a1912fb6f6bd5d6659a2ac555a229f
SARI Rim <rim.sari667762@gmail.com>
2025-10-21 23:50:34 +0200

    Ajout de commentaires SuperHero

--- src/SuperHero.ts
+++ src/SuperHero.ts
@@ -1,14 +1,16 @@
-// src/SuperHero.ts
+// Définition d'une classe pour représenter un super-héros
 export class SuperHero {
-  id: number;
-  name: string;
-  idApi: number;
-  slug?: string;
+  // Propriétés du super-héros
+  id: number;      // identifiant unique
+  name: string;    // nom du héros
+  idApi: number;   // identifiant dans une API externe
+  slug?: string;   // version courte du nom (optionnelle)
 
+  // Constructeur : permet d'initialiser les propriétés quand on crée un héros
   constructor(id: number, name: string, idApi: number, slug?: string) {
-    this.id = id;
-    this.name = name;
-    this.idApi = idApi;
-    this.slug = slug;
+    this.id = id;        // on affecte la valeur reçue à la propriété id
+    this.name = name;    // on affecte la valeur reçue à la propriété name
+    this.idApi = idApi;  // on affecte la valeur reçue à la propriété idApi
+    this.slug = slug;    // on affecte la valeur reçue à la propriété slug
   }
 }
